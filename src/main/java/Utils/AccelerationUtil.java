43811a090097ef9f75fd9d20c465bcc44fff4096
SergGurkov95 <Serg.Gurkov95@gmail.com>
2021-01-20 23:15:58 +0200

    1

--- src/main/java/Utils/AccelerationUtil.java
+++ src/main/java/Utils/AccelerationUtil.java
@@ -4,7 +4,7 @@ public class AccelerationUtil {
     final static double GRAVITATIONALCONSTANT = 6.6743 / Math.pow(10, 11);
 
     public static double freeFallAcceleration(double weight, double radius) {
-        return (GRAVITATIONALCONSTANT * weight * Math.pow(10, 24) / (radius * radius * Math.pow(10, 12)));//степени нужны что бы не захламлять массивы с данными 
+        return (GRAVITATIONALCONSTANT * weight * Math.pow(10, 24) / (radius * radius * Math.pow(10, 12)));//степени нужны что бы не захламлять массивы с данными
     }
 
 }
